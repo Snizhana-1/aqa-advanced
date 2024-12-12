86b449a9236a2a89d478a9f547206d230890d44d
Snizhana Morozkova <morozkova.sss@gmail.com>
2024-12-12 13:03:08 +0100

    A task for 2nd JS lesson

---
+++ jsBasics/task1.js
@@ -0,0 +1,61 @@
+//task 1
+
+let newString="aValue";
+let newNumber=99;
+let newBoolean=true;
+let newNull=null;
+let newUndefined;
+
+console.log(newString);
+console.log(newNumber);
+console.log(newBoolean);
+console.log(newNull);
+console.log(newUndefined);
+
+//task 2
+
+let name1= "Nana";
+let name2="Olga";
+let sayHi1="Hello "+name1+" and "+name2+"!";
+let sayHi2=`Hello ${name1} and ${name2}!`;
+
+console.log(sayHi1);
+console.log(sayHi2);
+
+//task 3
+
+let age=15;
+let isAdult=age>=18;
+
+console.log("Person's age",age);
+console.log("This person is already an adult:",isAdult);
+
+//task 4
+//4.1
+
+let radius = 5;
+let circleArea=Math.PI*Math.pow(radius,2);
+console.log("Area of ​​a circle: ",circleArea.toFixed(2));
+
+//4.2
+
+let length=15 ;
+let width=11 ;
+let rectangleArea=length*width;
+
+console.log("Area of ​​a rectangle: ",rectangleArea.toFixed(2));
+
+//4.3
+
+let radius0=12;
+let height0=17;
+let cylinderVolume=Math.PI*Math.pow(radius0,2)*height0;
+
+console.log("The cylinder volume is: ",cylinderVolume.toFixed(2));
+
+
+
+
+
+
+
